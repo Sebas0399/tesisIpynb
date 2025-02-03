615810de8bc55982c8b3729ced63a784c8f8bfa6
Sebas0399 <antony.aguay@gmail.com>
2025-02-02 19:38:17 -0800

    fix

--- SplitData.py
+++ SplitData.py
@@ -10,7 +10,7 @@ class SplitData:
         # Set test al val size
         self.test_size =float( config.get("dataset","test_size"))
         self.val_size=float(config.get("dataset","val_size"))
-        self.image_size = config.get("dataset","val_size")
+        self.image_size = config.get("dataset","image_size")
         self.color_space = config.get("dataset","color_mode")
         self.dataset_dir=config.get("dataset","base_dir")
     def split(self):
@@ -31,7 +31,7 @@ class SplitData:
                 # Dividir en train y temp (que luego se dividirá en val y test)
                 print(self.test_size*2)
                 train_images, temp_images = train_test_split(images, test_size=self.test_size*2, random_state=42)
-                val_images, test_images = train_test_split(temp_images, test_size=(self.val_size*2)+0.1, random_state=42)  # 20% para val y 20% para test
+                val_images, test_images = train_test_split(temp_images, test_size=(self.val_size*2), random_state=42)  # 20% para val y 20% para test
                 
                 # Crear carpetas de clase en train, val y test
                 os.makedirs(os.path.join(base_dir, 'train', class_name), exist_ok=True)
